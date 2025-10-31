5f6a4d31b333e3d0f5b53ad443aa9c12b67dd24d
harshil <harshil.desai@rapidfort.com>
2025-10-31 12:27:41 -0700

    parameterizing test files for GL pipeline.

--- tests/examples/kimia-docker-builds/examples/alpine.sh
+++ tests/examples/kimia-docker-builds/examples/alpine.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -Eeuo pipefail
-docker run --rm --cap-drop ALL --cap-add SETUID --cap-add SETGID --security-opt seccomp=unconfined --security-opt apparmor=unconfined ghcr.io/rapidfort/kimia:latest \
+docker run --rm --cap-drop ALL --cap-add SETUID --cap-add SETGID --security-opt seccomp=unconfined --security-opt apparmor=unconfined \
+  "$KIMIA_IMAGE" \
   --context=git://github.com/alpinelinux/docker-alpine.git \
   --dockerfile=Dockerfile \
   --destination=10.228.96.114:5000/my-alpine \
--- tests/examples/kimia-docker-builds/examples/postgres.sh
+++ tests/examples/kimia-docker-builds/examples/postgres.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -Eeuo pipefail
-docker run --rm --cap-drop ALL --cap-add SETUID --cap-add SETGID --security-opt seccomp=unconfined --security-opt apparmor=unconfined ghcr.io/rapidfort/kimia:latest \
+docker run --rm --cap-drop ALL --cap-add SETUID --cap-add SETGID --security-opt seccomp=unconfined --security-opt apparmor=unconfined \
+  "$KIMIA_IMAGE" \
   --context=https://github.com/docker-library/postgres.git \
   --context-sub-path=18/alpine3.22 \
   --dockerfile=Dockerfile \
