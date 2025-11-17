8cdead338b22496c0f3a1d2799f61d2b2f2c163f
harshil <harshil.desai@rapidfort.com>
2025-11-17 12:19:06 -0800

    debugging push ability for docker run tests in pipeline.

--- tests/examples/kimia-docker-builds/examples/alpine.sh
+++ tests/examples/kimia-docker-builds/examples/alpine.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -Eeuo pipefail
 docker run --rm --cap-drop ALL --cap-add SETUID --cap-add SETGID --security-opt seccomp=unconfined --security-opt apparmor=unconfined \
-  -v $HOME/.docker:/kimia/.docker:ro \
+  -v $HOME/.docker:home/kimia/.docker:ro \
   "$KIMIA_IMAGE" \
   --context=git://github.com/alpinelinux/docker-alpine.git \
   --dockerfile=Dockerfile \
