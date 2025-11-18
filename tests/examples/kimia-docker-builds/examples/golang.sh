741127009fe80e4981d179b067dbffddcef130cc
Nrupen Desai <nrupendesai@users.noreply.github.com>
2025-11-17 16:01:57 -0800

    Update golang.sh debug 2 - for testing docker login mount.

--- tests/examples/kimia-docker-builds/examples/golang.sh
+++ tests/examples/kimia-docker-builds/examples/golang.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -Eeuox pipefail
+
+ls -lla /root/.docker/config.json
+cat /root/.docker/config.json
+
 docker run --rm --cap-drop ALL --cap-add SETUID --cap-add SETGID --security-opt seccomp=unconfined --security-opt apparmor=unconfined \
-  -v "/root/.docker/config.json:/home/kimia/.docker/config.json:ro" \
+  -v "/root/.docker:/home/kimia/.docker:ro" \
   "$KIMIA_IMAGE" \
   --context=https://github.com/docker-library/golang.git \
   --context-sub-path=1.24/trixie \
