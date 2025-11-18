d0a51d352033e64a18dfa98b32dfa1e1c2c1e00a
harshil <harshil.desai@rapidfort.com>
2025-11-17 16:42:41 -0800

    correcting typo.

--- tests/examples/kimia-docker-builds/examples/golang.sh
+++ tests/examples/kimia-docker-builds/examples/golang.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -Eeuox pipefail
+
+ls -lla /root/.docker/config.json
+cat /root/.docker/config.json
+
 docker run --rm --cap-drop ALL --cap-add SETUID --cap-add SETGID --security-opt seccomp=unconfined --security-opt apparmor=unconfined \
-  -v "/root/.docker/config.json:/home/kimia/.docker/config.json:ro" \
+ -e DOCKER_CONFIG=/tmo/work/.docker/config.json \
+  -v "/root/.docker/config.json:/tmp/work/.docker/config.json" \
   "$KIMIA_IMAGE" \
   --context=https://github.com/docker-library/golang.git \
   --context-sub-path=1.24/trixie \
