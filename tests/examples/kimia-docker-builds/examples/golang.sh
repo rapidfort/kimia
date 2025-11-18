f27db7246fef2f7daef211a81ab2c79f184521f1
Nrupen Desai <nrupendesai@users.noreply.github.com>
2025-11-17 16:03:39 -0800

    disable read-only

--- tests/examples/kimia-docker-builds/examples/golang.sh
+++ tests/examples/kimia-docker-builds/examples/golang.sh
@@ -5,7 +5,7 @@ ls -lla /root/.docker/config.json
 cat /root/.docker/config.json
 
 docker run --rm --cap-drop ALL --cap-add SETUID --cap-add SETGID --security-opt seccomp=unconfined --security-opt apparmor=unconfined \
-  -v "/root/.docker:/home/kimia/.docker:ro" \
+  -v "/root/.docker:/home/kimia/.docker" \
   "$KIMIA_IMAGE" \
   --context=https://github.com/docker-library/golang.git \
   --context-sub-path=1.24/trixie \
