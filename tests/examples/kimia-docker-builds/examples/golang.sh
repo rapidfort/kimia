69f51b5228ebbb991a0c4335b47540e7d3982b23
Nrupen Desai <nrupendesai@users.noreply.github.com>
2025-11-17 17:02:36 -0800

    fix file path

--- tests/examples/kimia-docker-builds/examples/golang.sh
+++ tests/examples/kimia-docker-builds/examples/golang.sh
@@ -9,7 +9,7 @@ ls -lla /root/.docker/config.json
 cat /root/.docker/config.json
 
 docker run --rm --cap-drop ALL --cap-add SETUID --cap-add SETGID --security-opt seccomp=unconfined --security-opt apparmor=unconfined \
- -e DOCKER_CONFIG=/tmo/work/.docker/config.json \
+ -e DOCKER_CONFIG=/tmp/work/.docker/config.json \
   -v "/root/.docker/config.json:/tmp/work/.docker/config.json" \
   "$KIMIA_IMAGE" \
   --context=https://github.com/docker-library/golang.git \
