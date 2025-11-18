5fd3074e7649cc2619cce39aa8ce36307552a78e
Nrupen Desai <nrupendesai@users.noreply.github.com>
2025-11-17 17:19:51 -0800

    Update golang.sh

--- tests/examples/kimia-docker-builds/examples/golang.sh
+++ tests/examples/kimia-docker-builds/examples/golang.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 set -Eeuox pipefail
 
-ls -lla /root/.docker/config.json
-cat /root/.docker/config.json
-
-
+chmod 777 /root/.docker/config.json
 ls -lla /root/.docker/config.json
 cat /root/.docker/config.json
 
 docker run --rm --cap-drop ALL --cap-add SETUID --cap-add SETGID --security-opt seccomp=unconfined --security-opt apparmor=unconfined \
- -e DOCKER_CONFIG=/tmp/work/.docker/config.json \
-  -v "/root/.docker/config.json:/tmp/work/.docker/config.json" \
+  -v "/root/.docker:/home/kimia/.docker" \
   "$KIMIA_IMAGE" \
   --context=https://github.com/docker-library/golang.git \
   --context-sub-path=1.24/trixie \
